30a857ee9b18ab5937866cc65ea7969ffa8cdc33
shubhamaher11 <61388774+shubhamaher11@users.noreply.github.com>
2022-06-10 10:20:31 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "ap-south-1"
-  access_key = "AKIA56M4MQPX2YYXF7PP"
-  secret_key = "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"
+  access_key = "AKIA56M4MQPXRQ5XRNRT"
+  secret_key = "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"
 }
 
 resource "aws_s3_bucket" "first5" {
